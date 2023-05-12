b974f7c7532fe8acfc1b3da043ef7c4b54b49956
webgently <oleksandrshkabura811@gmail.com>
2023-05-12 03:06:32 +0300

    feat: add screen width check function

--- src/components/UserSidebar/index.tsx
+++ src/components/UserSidebar/index.tsx
@@ -2,9 +2,21 @@ import React from 'react';
 import IconMenu from '../../components/Icons';
 
 const UserSidebar = (props: any) => {
+  const getWidth = () => {
+    return window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+  };
+
+  React.useEffect(() => {
+    const setResponsiveness = () => {
+      getWidth() > 1024 && props.setOpen(true);
+    };
+    setResponsiveness();
+    window.addEventListener('resize', setResponsiveness);
+  }, []);
+
   return (
     <div className="user-sidebar">
-      <button className="hamburger-button" onClick={() => props.setOpen(!props.open)}>
+      <button className="hamburger-button" onClick={() => getWidth() < 1024 && props.setOpen(!props.open)}>
         <IconMenu icon="Hamburger" size={24} height={20} />
       </button>
       <div className="user-setting-list"></div>
